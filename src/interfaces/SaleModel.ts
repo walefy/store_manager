649ab52bf6c3180f278339f35b3f9e26b2e51944
walefy <walefyd1@gmail.com>
2024-06-14 10:03:11 -0300

    feat: add sales with product type (start orm change)

--- src/interfaces/SaleModel.ts
+++ src/interfaces/SaleModel.ts
@@ -1,11 +1,23 @@
+import type { Product } from './Product';
 import type { Sale } from './Sale';
 
 export type SaleWithoutId = Omit<Sale, 'id'>;
+export type SaleWithProduct = Sale & {
+  salesProducts: {
+    productId: number;
+    saleId: number;
+  }[];
+};
+
+export type SaleWithProducts = {
+  id: Sale['id'];
+  products: Product[];
+};
 
 export interface SaleModel {
   findAll(): Promise<Sale[]>;
   findById(id: Sale['id']): Promise<Sale | null>;
-  create(Sale: SaleWithoutId): Promise<unknown>;
+  create(Sale: SaleWithoutId, productIds: number[]): Promise<SaleWithProducts[]>;
   delete(id: Sale['id']): Promise<void>;
   update(id: Sale['id'], data: Partial<SaleWithoutId>): Promise<Sale>;
 }
